04a562fefa0ecd8636e2fa1855a636a16a25b7f2
Bruno Vasquez <brunomvasquez@gmail.com>
2021-09-26 20:05:29 -0300

    Criação das controllers para categoria e status

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -5,11 +5,63 @@ namespace ToDoServer.Controllers
 {
     [ApiController]
     [Route("api/category")]
-    public class CategoryController : ControllerBase {
+    public class CategoryController : ControllerBase
+    {
        private readonly DataContext _context;
-        public CategoryController(DataContext context){
+
+        public CategoryController(DataContext context)
+        {
             _context = context;
         }
 
+        //Post: api/category/create
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create(Category category)
+        {
+            _context.Category.Add (category);
+            _context.SaveChanges();
+            return Created("", category);
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public IActionResult List() => Ok(_context.Category.ToList());
+
+        [HttpGet]
+        [Route("getById/{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            Category category = _context.Category.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            Category category = _context.Category.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _context.Category.Remove (category);
+            _context.SaveChanges();
+            return Ok(_context.Category.ToList());
+        }
+
+        [HttpPut]
+        [Route("update/{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] Category category)
+        {
+            category.Id = id;
+            _context.Category.Update (category);
+            _context.SaveChanges();
+            return Ok(_context.Category.ToList());
+        }
      }
 }
--- Controllers/StatusController.cs
+++ Controllers/StatusController.cs
@@ -7,11 +7,61 @@ namespace ToDoServer.Controllers
     [Route("api/status")]
     public class StatusController : ControllerBase
     {
-        private readonly DataContext _context;
+       private readonly DataContext _context;
 
         public StatusController(DataContext context)
         {
             _context = context;
         }
-    }
+
+        //Post: api/status/create
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create(Status status)
+        {
+            _context.Status.Add (status);
+            _context.SaveChanges();
+            return Created("", status);
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public IActionResult List() => Ok(_context.Status.ToList());
+
+        [HttpGet]
+        [Route("getById/{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            Status status = _context.Status.Find(id);
+            if (status == null)
+            {
+                return NotFound();
+            }
+            return Ok(status);
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            Status status = _context.Status.Find(id);
+            if (status == null)
+            {
+                return NotFound();
+            }
+            _context.Status.Remove (status);
+            _context.SaveChanges();
+            return Ok(_context.Status.ToList());
+        }
+
+        [HttpPut]
+        [Route("update/{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] Status status)
+        {
+            status.Id = id;
+            _context.Status.Update (status);
+            _context.SaveChanges();
+            return Ok(_context.Status.ToList());
+        }
+     }
 }
